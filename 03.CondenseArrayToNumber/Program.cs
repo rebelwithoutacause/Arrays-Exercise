d10c7f899aaa06ac261fa3c0f28a75e4291540a4
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-05 08:49:07 +0200

    Add files via upload

---
+++ 03.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+         
+       
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+
+         
+        int[] numbers = Array.ConvertAll(inputArray, int.Parse);
+
+        
+        while (numbers.Length > 1)
+        {
+            int[] condensed = new int[numbers.Length - 1];
+
+             
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                condensed[i] = numbers[i] + numbers[i + 1];
+            }
+
+             
+            numbers = condensed;
+        }
+
+        
+        Console.WriteLine(numbers[0]);
+    }
+}
---
+++ 04.MagicSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+         
+        int[] numbers = Console.ReadLine()
+                               .Split()
+                               .Select(int.Parse)
+                               .ToArray();
+
+        
+        int controlNumber = int.Parse(Console.ReadLine());
+
+        
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                if (numbers[i] + numbers[j] == controlNumber)
+                {
+                    Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                }
+            }
+        }
+    }
+}
