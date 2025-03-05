0d2402dc933e4ab13fce1af2e8c4a9f0ccbc3cfd
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-05 08:50:06 +0200

    Add files via upload

---
+++ 06Array.Rotation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] numbers = Console.ReadLine()
+                               .Split()
+                               .Select(int.Parse)
+                               .ToArray();
+
+        
+        int rotations = int.Parse(Console.ReadLine());
+
+         
+        rotations = rotations % numbers.Length;
+
+         
+        for (int i = 0; i < rotations; i++)
+        {
+            
+            int firstNumber = numbers[0];
+            for (int j = 1; j < numbers.Length; j++)
+            {
+                numbers[j - 1] = numbers[j];
+            }
+            numbers[numbers.Length - 1] = firstNumber;
+        }
+
+         
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
---
+++ 07.TopIntegers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isTopNumber = true;
+                int currentNumber = numbers[i];
+
+                 
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (currentNumber <= numbers[j])
+                    {
+                        isTopNumber = false;
+                        break;
+                    }
+                }
+
+                
+                if (isTopNumber)
+                {
+                    Console.Write($"{currentNumber} ");
+                }
+            }
+        }
+    }
+}
