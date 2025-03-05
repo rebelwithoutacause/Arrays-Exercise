ee1dcf678650bee9898e09a5adf23678fd352e91
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-05 08:48:06 +0200

    Add files via upload

---
+++ 01.EqualArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] array1 = Console.ReadLine().Split(' ');
+        string[] array2 = Console.ReadLine().Split(' ');
+
+        
+        if (array1.Length == array2.Length)
+        {
+            bool areIdentical = true;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    areIdentical = false;
+                    break;
+                }
+            }
+
+            if (areIdentical)
+            {
+                Console.WriteLine("Arrays are identical.");
+            }
+            else
+            {
+                Console.WriteLine("Arrays are not identical.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Arrays are not identical.");
+        }
+    }
+}
---
+++ 02.CommonElements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] array1 = Console.ReadLine().Split();
+        string[] array2 = Console.ReadLine().Split();
+
+        
+        foreach (string num1 in array1)
+        {
+            foreach (string num2 in array2)
+            {
+                if (num1 == num2)
+                {
+                    Console.Write(num1 + " ");
+                }
+            }
+        }
+    }
+}
